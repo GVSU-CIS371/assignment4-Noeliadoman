b74d18f6645c1b5c97d22f8fdbb317fa8581faf0
Noelia <chaevzn@mail.gvsu.edu>
2026-03-27 20:27:58 -0400

    added previous files

--- src/components/Base.vue
+++ src/components/Base.vue
@@ -2,16 +2,21 @@
   <div class="baseBeverage"></div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { computed } from "vue";
+import { currentBase } from "../stores/beverage";
+const bgColor = computed(() => currentBase.value.color);
+</script>
 
 <style scoped>
 .baseBeverage {
+  background-color: v-bind(bgColor);
   position: relative;
   width: 100%;
   height: 100%;
   bottom: 0;
   animation: pour-tea 2s;
   z-index: 300;
-  /* // border-radius: 0.05em 0.05em 2.2em 2.2em; */
 }
 </style>
+
